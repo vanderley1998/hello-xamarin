7ab87bf1fae90d74c52a9421af2b8056227976ee
Vanderley Junior <vanderley_1998@hotmail.com.br>
2019-05-30 18:18:51 -0300

    listas, binding e displayAlert

--- HelloXamarin/HelloXamarin/MainPage.xaml.cs
+++ HelloXamarin/HelloXamarin/MainPage.xaml.cs
@@ -7,18 +7,37 @@ using Xamarin.Forms;
 
 namespace HelloXamarin
 {
+    public class Veiculo
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string FormattedPrice { get { return string.Format("R$ {0}", Price); } }
+    }
+
     public partial class MainPage : ContentPage
     {
+
+        public List<Veiculo> Veiculos { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
 
-            listViewVeiculos.ItemsSource = new string[]
+            this.Veiculos = new List<Veiculo>
             {
-                "Azera V6",
-                "Fiesta 2.0",
-                "HB20S"
-            }; 
+                new Veiculo { Name = "Azera v6", Price = 60000},
+                new Veiculo { Name = "Fiesta 2.0", Price = 34000},
+                new Veiculo { Name = "HB20S", Price = 25000}
+            };
+
+            // listViewVeiculos.ItemsSource = this.Veiculos; --> taguiado na view
+            this.BindingContext = this;
+        }
+
+        private void ListViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo) e.Item;
+            DisplayAlert("Test drive", string.Format("Você tocou no modelo '{0}', que custa {1}", veiculo.Name, veiculo.FormattedPrice), "OK");
         }
     }
 }
